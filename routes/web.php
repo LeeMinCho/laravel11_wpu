f72827214152f8c31a4fd455735c7a3dca1bac69
Eka Yunan <ekayunan98@gmail.com>
2024-05-09 16:11:12 +0700

    feat(Blog): Single Post

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,38 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Arr;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+$posts = [
+    [
+        'id' => 1,
+        'slug' => 'judul-artikel-1',
+        'title' => 'Judul Artikel 1',
+        'author' => 'Eka Yunan',
+        'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut possimus eum earum tempora exercitationem quaerat voluptate voluptatibus repellendus debitis explicabo rerum dicta, et atque, libero error! Magni atque id velit!'
+    ],
+    [
+        'id' => 2,
+        'slug' => 'judul-artikel-2',
+        'title' => 'Judul Artikel 2',
+        'author' => 'Eka Yunan',
+        'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque temporibus, soluta voluptates alias necessitatibus quod ullam deserunt provident omnis maxime officiis? Minus inventore voluptatum ab ducimus earum dolorum molestiae odio?'
+    ],
+];
+Route::get('/posts', function () use ($posts) {
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
+});
+
+Route::get('/posts/{slug}', function ($slug) use ($posts) {
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => $post['title'], 'post' => $post]);
 });
 
 Route::get('/about', function () {
