7376bac8576b0297eda4e2cae25b0a842953acda
Eka Yunan <ekayunan98@gmail.com>
2024-05-16 20:30:29 +0700

    feat(Post): Belajar Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,39 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Eka Yunan',
-                'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut possimus eum earum tempora exercitationem quaerat voluptate voluptatibus repellendus debitis explicabo rerum dicta, et atque, libero error! Magni atque id velit!'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Eka Yunan',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque temporibus, soluta voluptates alias necessitatibus quod ullam deserunt provident omnis maxime officiis? Minus inventore voluptatum ab ducimus earum dolorum molestiae odio?'
-            ],
-        ];
-    }
+    use HasFactory;
 
-    public static function find($slug): array
-    {
-        /* return Arr::first(static::all(), function ($post) use ($slug) {
-            return $post['slug'] == $slug;
-        }); */
-        $post =  Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
-        if (!$post) {
-            abort(404);
-        }
-        return $post;
-    }
+    protected $fillable = ['title', 'slug', 'author', 'body'];
 }
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,7 @@ Route::get('/posts', function () {
     return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 });
 
-Route::get('/posts/{slug}', function ($slug) {
-    $post = Post::find($slug);
+Route::get('/posts/{post:slug}', function (Post $post) {
     return view('post', ['title' => $post['title'], 'post' => $post]);
 });
 
