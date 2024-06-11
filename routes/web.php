fdc0b0a7f4f49b42a6cf067c823d8f5e47228114
Eka Yunan <ekayunan98@gmail.com>
2024-06-11 20:53:41 +0700

    feat(Post): Eager Loading

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,9 @@ Route::get('/', function () {
 });
 
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+    // $posts = Post::with('author', 'category')->get();
+    $posts = Post::all();
+    return view('posts', ['title' => 'Blog', 'posts' => $posts]);
 });
 
 Route::get('/posts/{post:slug}', function (Post $post) {
@@ -18,10 +20,12 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 });
 
 Route::get('/authors/{user:username}', function (User $user) {
+    // $posts = $user->posts->load('author', 'category');
     return view('posts', ['title' => count($user->posts) . ' Articles by: ' . $user['name'], 'posts' => $user->posts]);
 });
 
 Route::get('/categories/{category}', function (Category $category) {
+    // $posts = $category->posts->load('author', 'category');
     return view('posts', ['title' => 'Articles in: ' . $category['name'], 'posts' => $category->posts]);
 });
 
